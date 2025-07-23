528ecbe82519d37f9965b3ce35bb747a33927873
Seth Waltemyer <v-sewalt@microsoft.com>
2025-07-23 16:12:01 -0700

    Remove delete button

--- frontend/src/components/CustomEdge.tsx
+++ frontend/src/components/CustomEdge.tsx
@@ -1,12 +1,9 @@
 import { memo } from 'react';
 import {
   BaseEdge,
-  EdgeLabelRenderer,
   getStraightPath,
   type EdgeProps,
 } from 'reactflow';
-import { Button } from './ui/button';
-import { Cross1Icon } from '@radix-ui/react-icons';
 
 interface CustomEdgeProps extends EdgeProps {
   data?: {
@@ -24,8 +21,7 @@ function CustomEdge({
   markerEnd,
   data,
 }: CustomEdgeProps) {
-  const deleteEdge = data?.deleteEdge;
-  const [edgePath, labelX, labelY] = getStraightPath({
+  const [edgePath] = getStraightPath({
     sourceX,
     sourceY,
     targetX,
@@ -35,28 +31,6 @@ function CustomEdge({
   return (
     <>
       <BaseEdge path={edgePath} markerEnd={markerEnd} style={style} />
-      <EdgeLabelRenderer>
-        <div
-          style={{
-            position: 'absolute',
-            transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
-            fontSize: 12,
-            pointerEvents: 'all',
-          }}
-          className="nodrag nopan"
-        >
-          {deleteEdge && (
-            <Button
-              className="h-6 w-6 rounded-full"
-              size="icon"
-              variant="outline"
-              onClick={() => deleteEdge(id)}
-            >
-              <Cross1Icon className="h-3 w-3" />
-            </Button>
-          )}
-        </div>
-      </EdgeLabelRenderer>
     </>
   );
 }
